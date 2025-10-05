cb11adabfcf926e3f82968eeefdb371a45e9826e
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-10-05 22:09:50 +0200

    Add new foldera and new jsx file with component.

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+function NavBar() {
+  return (
+    <>
+      <h1>Movie Watch</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Movies</li>
+          <li>Coming</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
