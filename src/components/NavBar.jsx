289418d7fbbe78e97db563f913b075b623c563cb
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-10-13 21:32:48 +0200

    App sign in modal functionality.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "./SearchBar";
+import SignInModal from "./SignInModal";
 
 function NavBar({ onSearch }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleSignInClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <header className="navbar">
-      <div className="navbar-left">
-        <h1 className="brand">
-          <span className="clipper-cut-emoji">🎬</span> Movie Watch
-        </h1>
-      </div>
+    <>
+      <header className="navbar">
+        <div className="navbar-left">
+          <h1 className="brand">
+            <span className="clipper-cut-emoji">🎬</span> Movie Watch
+          </h1>
+        </div>
 
-      <div className="navbar-center" />
+        <div className="navbar-center" />
 
-      <div className="navbar-right">
-        <nav className="main-nav">
-          <a href="#">Home</a>
-          <a href="#">Movies</a>
-          <a href="#">Coming</a>
-        </nav>
+        <div className="navbar-right">
+          <nav className="main-nav">
+            <a href="#">Home</a>
+            <a href="#">Movies</a>
+            <a href="#">Coming</a>
+          </nav>
 
-        <SearchBar onSearch={onSearch} />
+          <SearchBar onSearch={onSearch} />
 
-        <button className="sign-in-button">Sign in</button>
-      </div>
-    </header>
+          <button className="sign-in-button" onClick={handleSignInClick}>
+            Sign in
+          </button>
+        </div>
+      </header>
+      <SignInModal isOpen={isModalOpen} onClose={handleCloseModal} />
+    </>
   );
 }
 
